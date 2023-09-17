f292897ab4f6bb2951f0a33a225c7d991582628e
AndreMotta25 <andremotta16041998@gmail.com>
2023-09-17 02:06:59 -0300

    ✨ feat(pendingfamiliar): add repository to pendingFamiliar

---
+++ src/modules/users/repositories/implementations/PendingFamiliarRepository.ts
@@ -0,0 +1,35 @@
+import { database } from 'src/database/index';
+import { Repository } from 'typeorm';
+
+import { PendingFamiliar } from '../../entities/PendingFamiliar';
+import {
+  ICreateFamiliar,
+  IPendingFamiliarRepository,
+} from '../IPendingFamiliarRepository';
+
+class PendingFamiliarRepository implements IPendingFamiliarRepository {
+  private repository: Repository<PendingFamiliar>;
+
+  constructor() {
+    this.repository = database.getRepository(PendingFamiliar);
+  }
+  async findInvitationsByUser(id: string): Promise<PendingFamiliar[]> {
+    const invitations = await this.repository.find({
+      where: { userId: id },
+      relations: { user_pending: true },
+    });
+    return invitations;
+  }
+
+  async create({ kin, user }: ICreateFamiliar): Promise<PendingFamiliar> {
+    const invitationPending = this.repository.create({
+      user,
+      user_pending: kin,
+    });
+    await this.repository.save(invitationPending);
+
+    return invitationPending;
+  }
+}
+
+export { PendingFamiliarRepository };
