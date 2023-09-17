7aac24867e7cd50d11ce4e9f3a83e582ff854fc4
AndreMotta25 <andremotta16041998@gmail.com>
2023-09-17 02:08:21 -0300

    🧹 chore(database): add dataSource

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,12 +1,18 @@
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
 
 const database = new DataSource({
-    type:'postgres',
-    host: process.env.HOST,
-    port: Number(process.env.PORT_DB),
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
-}) 
+  type: 'postgres',
+  host: process.env.HOST,
+  port: Number(process.env.PORT_DB),
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  entities: [
+    './src/modules/users/entities/*.ts',
+    './src/modules/shared/entities/*.ts',
+  ],
+  migrations: ['./src/database/migrations/*.ts'],
+});
 
-export {database}
+export { database };
