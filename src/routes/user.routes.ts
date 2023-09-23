a8986a11df0bb7f6ad7a4d76d38c34d892661705
AndreMotta25 <andremotta16041998@gmail.com>
2023-09-23 12:54:10 -0300

    ♻️ refactor(route/user): leaving a single responsibility which is to work only with the user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,16 +1,12 @@
 import { Router } from 'express';
-import { isAuthenticated } from 'src/middleware/isAuthenticated';
+// import { isAuthenticated } from 'src/middleware/isAuthenticated';
 
 import { CreateUserController } from '@modules/users/useCases/CreateUser/CreateUserController';
-import { InviteFamiliarController } from '@modules/users/useCases/InvitationFamiliar/InviteFamiliarController';
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
-const inviteFamiliarController = new InviteFamiliarController();
 
 userRoutes.post('/', createUserController.handle);
-
-userRoutes.post('/invite', isAuthenticated, inviteFamiliarController.handle);
 
 export { userRoutes };
