db4ca55033f637479952662372ab4f2cfe458f00
AndreMotta25 <andremotta16041998@gmail.com>
2023-09-29 21:18:42 -0300

    ✨ feat(tsyringe): add dependency of list and familyMembers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,10 @@
 import { container } from 'tsyringe';
 
+import { IListRepository } from '@modules/list/repositories/IListRepository';
+import { ListRepository } from '@modules/list/repositories/implements/ListRepository';
+import { IFamilyMembersRepository } from '@modules/users/repositories/IFamilyMembersRepository';
 import { IInvitationsRepository } from '@modules/users/repositories/IInvitationsRepository';
+import { FamilyMembersRepository } from '@modules/users/repositories/implementations/FamilyMembersRepository';
 import { InvitationsRepository } from '@modules/users/repositories/implementations/InvitationsRepository';
 import { UserRepository } from '@modules/users/repositories/implementations/UserRepository';
 import { IUserRepository } from '@modules/users/repositories/IUserRepository';
@@ -9,4 +13,9 @@ container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 container.registerSingleton<IInvitationsRepository>(
   'InvitationRepository',
   InvitationsRepository,
+);
+container.registerSingleton<IListRepository>('ListRepository', ListRepository);
+container.registerSingleton<IFamilyMembersRepository>(
+  'FamilyMembersRepository',
+  FamilyMembersRepository,
 );
