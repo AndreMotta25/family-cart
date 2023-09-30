4ecd53e9ba0ce6fa4bc66a1bf5cb91ac86a34400
AndreMotta25 <andremotta16041998@gmail.com>
2023-09-29 21:16:45 -0300

    ✨ feat(invite/routes): add route accept
    an route that accept a invitation

--- src/routes/invite.routes.ts
+++ src/routes/invite.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { isAuthenticated } from 'src/middleware/isAuthenticated';
 
+import { AcceptInviteController } from '@modules/users/useCases/AcceptInvite/AcceptInviteController';
 import { InviteFamiliarController } from '@modules/users/useCases/InvitationFamiliar/InviteFamiliarController';
 import { ViewInvitationController } from '@modules/users/useCases/ViewInvitation/ViewInvitationController';
 
@@ -8,8 +9,11 @@ const inviteRoutes = Router();
 
 const inviteFamiliarController = new InviteFamiliarController();
 const viewInvitationController = new ViewInvitationController();
+const acceptInviteController = new AcceptInviteController();
 
 inviteRoutes.post('/', isAuthenticated, inviteFamiliarController.handle);
+inviteRoutes.post('/accept', isAuthenticated, acceptInviteController.handle);
+
 inviteRoutes.get('/', isAuthenticated, viewInvitationController.handle);
 
 export { inviteRoutes };
