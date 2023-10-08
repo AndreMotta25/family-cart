fe66d52e31d1f55a38ce82a16229fb61bbfb5d0d
AndreMotta25 <andremotta16041998@gmail.com>
2023-10-08 14:17:44 -0300

    ✨ feat(users/routes): add endpoints to remove a friend and get info from user authenticate

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
-// import { isAuthenticated } from 'src/middleware/isAuthenticated';
+import { isAuthenticated } from 'src/middleware/isAuthenticated';
 
+import { CancelFriendshipController } from '@modules/users/useCases/CancelFriendship/CancelFriendshipController';
 import { CreateUserController } from '@modules/users/useCases/CreateUser/CreateUserController';
+import { GetFriendsController } from '@modules/users/useCases/GetFriends/GetFriendsController';
+import { GetUserController } from '@modules/users/useCases/GetUser/GetUserController';
 
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
+const getUserController = new GetUserController();
+const cancelFriendshipController = new CancelFriendshipController();
+const getFriendsController = new GetFriendsController();
 
 userRoutes.post('/', createUserController.handle);
+userRoutes.get('/', isAuthenticated, getUserController.handle);
+userRoutes.delete(
+  '/cancel_friendship',
+  isAuthenticated,
+  cancelFriendshipController.handle,
+);
+userRoutes.get('/friends', isAuthenticated, getFriendsController.handle);
 
 export { userRoutes };
