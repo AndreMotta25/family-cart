f50ac7e5cf1a99cece53b285b98fca81a3dc8264
AndreMotta25 <andremotta16041998@gmail.com>
2023-10-08 20:58:49 -0300

    ♻️ refactor(list): using a dto

--- src/modules/list/useCases/GetList/GetListUseCase.ts
+++ src/modules/list/useCases/GetList/GetListUseCase.ts
@@ -3,19 +3,45 @@ import { inject, injectable } from 'tsyringe';
 import { AppError } from '@errors/AppError';
 import { IListRepository } from '@modules/list/repositories/IListRepository';
 
+interface IItem {
+  id: string;
+  name: string;
+  quantity: number;
+  url: string;
+}
+interface IListResponse {
+  id: string;
+  name: string;
+  created_at: Date;
+  update_at: Date;
+  itens: IItem[];
+}
 @injectable()
 class GetListUseCase {
   constructor(
     @inject('ListRepository') private listRepository: IListRepository,
   ) {}
 
-  async execute(id: string) {
+  async execute(id: string): Promise<IListResponse> {
     const list = await this.listRepository.getList(id);
 
     if (!list)
       throw new AppError({ message: 'List Not Found', statusCode: 404 });
 
-    return list;
+    const itens = list.itens.map((item) => ({
+      id: item.id,
+      name: item.name,
+      quantity: item.quantity,
+      url: item.url,
+    }));
+
+    return {
+      id: list.id,
+      name: list.name,
+      created_at: list.created_at,
+      update_at: list.update_at,
+      itens,
+    };
   }
 }
 
