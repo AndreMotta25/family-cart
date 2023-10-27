ead2008cd700f5a56700a81238c757dfb509a06d
AndreMotta25 <andremotta16041998@gmail.com>
2023-10-26 23:21:32 -0300

    ✨ feat(user): make relations with sharedList

--- src/modules/users/entities/User.ts
+++ src/modules/users/entities/User.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Column, Entity, JoinTable, ManyToMany, OneToMany } from 'typeorm';
 
+import { SharedList } from '@modules/list/entities/SharedList';
+
 import { Entity as Parent } from '../../shared/entities/Entity';
 import { FamilyMember } from './FamilyMember';
 
@@ -18,9 +20,20 @@ class User extends Parent {
   @OneToMany(() => FamilyMember, (u) => u.user)
   friends: FamilyMember[];
 
+  @OneToMany(() => SharedList, (s) => s.owner)
+  sharedList: SharedList[];
+
+  @OneToMany(() => SharedList, (s) => s.additionalUser)
+  sharedListInverse: SharedList[];
+
   constructor() {
     super();
   }
 }
 
 export { User };
+
+/*
+  Um usuario pode ter n listas compartilhadas
+  por isso que aponta para owner
+*/
