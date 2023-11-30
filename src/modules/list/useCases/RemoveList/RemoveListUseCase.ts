32dbaabca317fe5cb7906cf4632a55158fd01fa8
AndreMotta25 <andremotta16041998@gmail.com>
2023-11-30 15:39:09 -0300

    ✨ feat: notifying when a list is deleted

--- src/modules/list/useCases/RemoveList/RemoveListUseCase.ts
+++ src/modules/list/useCases/RemoveList/RemoveListUseCase.ts
@@ -1,7 +1,10 @@
 import { inject, injectable } from 'tsyringe';
 
 import { AppError } from '@errors/AppError';
+import { List } from '@modules/list/entities/List';
 import { IListRepository } from '@modules/list/repositories/IListRepository';
+import { IShareListRepository } from '@modules/list/repositories/IShareListRepository';
+import { INotificationRepository } from '@modules/notify/repositories/INotificationRepository';
 import { IUserRepository } from '@modules/users/repositories/IUserRepository';
 
 interface IRemoveListResquest {
@@ -14,11 +17,15 @@ class RemoveListUseCase {
   constructor(
     @inject('UserRepository') private userRepository: IUserRepository,
     @inject('ListRepository') private listRepository: IListRepository,
+    @inject('ShareListRepository')
+    private shareListRepository: IShareListRepository,
+    @inject('NotificationRepository')
+    private notifyRepository: INotificationRepository,
   ) {}
 
   async execute({ owner_id, id_list }: IRemoveListResquest) {
     const ownerExists = await this.userRepository.findById(owner_id);
-    const listExists = await this.listRepository.findListById(id_list);
+    const listExists = (await this.listRepository.getList(id_list)) as List;
 
     if (!ownerExists)
       throw new AppError({ message: 'User Not Found', statusCode: 404 });
@@ -28,6 +35,23 @@ class RemoveListUseCase {
 
     if (!(ownerExists.id === listExists.owner.id))
       throw new AppError({ message: 'Only the owner can remove the list' });
+
+    const sharedWith =
+      await this.shareListRepository.findShareByListWithRelations(id_list);
+
+    if (sharedWith.length > 0) {
+      const receptors = sharedWith.map((shared) => shared.additionalUserId);
+      await this.notifyRepository.create(
+        {
+          read: false,
+          emitterId: listExists.owner.id,
+          entity_id: listExists.id,
+          entity_name: 'list',
+          type: 'removeList',
+        },
+        receptors,
+      );
+    }
 
     await this.listRepository.remove(id_list);
   }
