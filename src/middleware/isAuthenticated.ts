029a4eaa45e8079072f96a3d18845786cb5170c9
AndreMotta25 <andremotta16041998@gmail.com>
2024-02-28 20:59:50 -0300

    ♻️ refactor: using a hashToken
    checking whether the hashToken is the same as when the token was created.

--- src/middleware/isAuthenticated.ts
+++ src/middleware/isAuthenticated.ts
@@ -7,6 +7,7 @@ import { UserRepository } from '@modules/users/repositories/implementations/User
 
 interface IJwt {
   subject: string;
+  hashToken: string;
 }
 
 const isAuthenticated = async (
@@ -15,20 +16,23 @@ const isAuthenticated = async (
   next: NextFunction,
 ) => {
   const { authorization } = request.headers;
-
   if (!authorization)
     throw new AppError({ message: 'Falta o token', statusCode: 401 });
 
   const [, token] = authorization.split(' ');
+
   try {
-    const { subject } = jwtDecode(token) as IJwt;
+    const { subject, hashToken } = jwtDecode(token) as IJwt;
     const userRepository = new UserRepository();
     const user = await userRepository.findById(subject);
 
     if (!user)
       throw new AppError({ message: 'Usuario não achado', statusCode: 401 });
 
-    verify(token, '1234');
+    verify(token, String(process.env.Secret));
+
+    if (user.hash !== hashToken)
+      throw new AppError({ message: 'Token Invalido', statusCode: 401 });
 
     request.user = {
       id: user.id,
