927cae81a3cbf00b82a087169e136ca01012933a
AndreMotta25 <andremotta16041998@gmail.com>
2024-02-28 21:19:03 -0300

    ♻️ refactor: add some inject dependencies

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,7 +1,19 @@
 import { container } from 'tsyringe';
 
+import { IItemRepository } from '@modules/list/repositories/IItemRepository';
 import { IListRepository } from '@modules/list/repositories/IListRepository';
+import { ItemRepository } from '@modules/list/repositories/implements/ItemRepository';
 import { ListRepository } from '@modules/list/repositories/implements/ListRepository';
+import { PendingListRepository } from '@modules/list/repositories/implements/PendingListRepository';
+import { ShareListRepository } from '@modules/list/repositories/implements/ShareListRepository';
+import { IPendingListRepository } from '@modules/list/repositories/IPendingListRepository';
+import { IShareListRepository } from '@modules/list/repositories/IShareListRepository';
+import { NotificationRepository } from '@modules/notify/repositories/implements/NotificationRepository';
+import { SseConnectRepository } from '@modules/notify/repositories/implements/SseConnectRepository';
+import { INotificationRepository } from '@modules/notify/repositories/INotificationRepository';
+import { ISseConnectRepository } from '@modules/notify/repositories/ISseConnectRepository';
+import { INotifyUseCase } from '@modules/notify/useCases/Notify/INotifyUseCase';
+import { NotifyUseCase } from '@modules/notify/useCases/Notify/NotifyUseCase';
 import { IFamilyMembersRepository } from '@modules/users/repositories/IFamilyMembersRepository';
 import { IInvitationsRepository } from '@modules/users/repositories/IInvitationsRepository';
 import { FamilyMembersRepository } from '@modules/users/repositories/implementations/FamilyMembersRepository';
@@ -10,12 +22,33 @@ import { UserRepository } from '@modules/users/repositories/implementations/User
 import { IUserRepository } from '@modules/users/repositories/IUserRepository';
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+container.registerSingleton<IPendingListRepository>(
+  'PendingListRepository',
+  PendingListRepository,
+);
+container.registerSingleton<IShareListRepository>(
+  'ShareListRepository',
+  ShareListRepository,
+);
+container.registerSingleton<IItemRepository>('ItemRepository', ItemRepository);
 container.registerSingleton<IInvitationsRepository>(
   'InvitationRepository',
   InvitationsRepository,
 );
 container.registerSingleton<IListRepository>('ListRepository', ListRepository);
+
+container.registerSingleton<INotificationRepository>(
+  'NotificationRepository',
+  NotificationRepository,
+);
+
 container.registerSingleton<IFamilyMembersRepository>(
   'FamilyMembersRepository',
   FamilyMembersRepository,
 );
+
+container.registerSingleton<ISseConnectRepository>(
+  'SseRepository',
+  SseConnectRepository,
+);
+container.registerSingleton<INotifyUseCase>('NotifyUser', NotifyUseCase);
