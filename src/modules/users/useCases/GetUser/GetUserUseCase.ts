5b28b482123e328ad8593852d0dbb2c72d8b046f
AndreMotta25 <andremotta16041998@gmail.com>
2024-02-28 21:17:22 -0300

    ♻️ refactor: editing return information

--- src/modules/users/useCases/GetUser/GetUserUseCase.ts
+++ src/modules/users/useCases/GetUser/GetUserUseCase.ts
@@ -1,12 +1,21 @@
 import { inject, injectable } from 'tsyringe';
 
 import { AppError } from '@errors/AppError';
+import { IShareListRepository } from '@modules/list/repositories/IShareListRepository';
+import { INotificationRepository } from '@modules/notify/repositories/INotificationRepository';
+import { IFamilyMembersRepository } from '@modules/users/repositories/IFamilyMembersRepository';
 import { IUserRepository } from '@modules/users/repositories/IUserRepository';
 
 @injectable()
 class GetUserUseCase {
   constructor(
     @inject('UserRepository') private userRepository: IUserRepository,
+    @inject('NotificationRepository')
+    private notificationRepository: INotificationRepository,
+    @inject('FamilyMembersRepository')
+    private familyMemberRepository: IFamilyMembersRepository,
+    @inject('ShareListRepository')
+    private shareListRepository: IShareListRepository,
   ) {}
 
   async execute(id: string) {
@@ -15,7 +24,23 @@ class GetUserUseCase {
     if (!user)
       throw new AppError({ message: 'User Not Found', statusCode: 404 });
 
-    return user;
+    const totalNotifications =
+      await this.notificationRepository.totalOfNotifications(user.id);
+
+    const qtdFriends = await this.familyMemberRepository.getTotalFriends(
+      user.id,
+    );
+    const listsSharedTotal = (
+      await this.shareListRepository.findShareByGuest(user.id)
+    ).length;
+
+    return {
+      name: user.name,
+      image: '',
+      totalNotifications,
+      totalFriends: qtdFriends,
+      listsSharedTotal,
+    };
   }
 }
 
