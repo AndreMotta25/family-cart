3c05f73555d5458da57d4e427a20c89eef179463
AndreMotta25 <andremotta16041998@gmail.com>
2024-11-08 20:52:12 -0300

    ♻️ refactor: change url connection database

--- src/database/index.ts
+++ src/database/index.ts
@@ -5,21 +5,22 @@ import { SeederOptions } from 'typeorm-extension';
 let extension = 'ts';
 
 if (process.env.NODE_ENV?.toLowerCase() === 'production') extension = 'js';
-
+console.log(extension);
 const options: DataSourceOptions & SeederOptions = {
   type: 'postgres',
-  host: process.env.HOST,
-  port: Number(process.env.PORT_DB),
-  username: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  // host: process.env.HOST,
+  // port: Number(process.env.PORT_DB),
+  // username: process.env.USER,
+  // password: process.env.PASSWORD,
+  // database: process.env.DATABASE,
+  url: 'postgresql://postgres.ikxaojmvahbzvslllafp:Desenhos12@@aws-0-us-west-1.pooler.supabase.com:6543/postgres',
   entities: [
     `./src/modules/users/entities/*.${extension}`,
     `./src/modules/list/entities/*.${extension}`,
     `./src/modules/shared/entities/*.${extension}`,
     `./src/modules/notify/entities/*.${extension}`,
   ],
-  migrations: ['./src/database/migrations/*.ts'],
+  migrations: [`./src/database/migrations/*.${extension}`],
   seeds: [],
 };
 
